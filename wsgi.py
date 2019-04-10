03040b93ff5d36efcebd4cf0d79fde4f671f2377
Ryan Hennessy <ryan.hennessy@datadoghq.com>
2019-04-09 21:40:38 -0500

    Fixed a typo with the name application in wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -4,15 +4,15 @@ from flask import Flask, jsonify
 
 application = Flask(__name__)
 
-@applicaton.route("/")
+@application.route("/")
 def home():
    return jsonify({'a': 1, 'b': 2, 'c': 3}) 
 
-@applicaton.route("/app")
+@application.route("/app")
 def apphome():
    return jsonify({'a': 4, 'b': 5, 'c': 6}) 
 
 
 if __name__ == "__main__":
-   applicaton.run()
+   application.run()
 
